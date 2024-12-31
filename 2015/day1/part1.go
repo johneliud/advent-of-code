78290747b35d36f923b09496ad7f5313e434d324
John <johneliud4@gmail.com>
2024-12-31 14:26:27 +0300

    add: findFloor function that ranges over a string and uses conditional statements to determine the floor.

---
+++ 2015/day1/part1.go
@@ -0,0 +1,16 @@
+package main
+
+func findFloor(s string) int {
+	var floor int
+
+	for _, char := range s {
+		if char == '(' {
+			floor++
+		} else if char == ')' {
+			floor--
+		} else {
+			continue
+		}
+	}
+	return floor
+}
