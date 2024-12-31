3e457cbcf306a5bff92816b0670ef3ec07658cbd
John <johneliud4@gmail.com>
2024-12-31 14:28:50 +0300

    add: entry point function for the program that invokes ReadInput, determineSquareFeetOfWrappingPaper and determineFeetOfRibbon functions.

---
+++ 2015/day2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"advent-of-code/utils"
+	"fmt"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	start := time.Now()
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		fmt.Printf("Failed loading .env file: %v\n", err)
+		return
+	}
+
+	input := utils.ReadInput("2015", "2")
+
+	fmt.Println("Square feet of wrapping paper:", determineSquareFeetOfWrappingPaper(input))
+	fmt.Println("Feet of ribbon:", determineFeetOfRibbon(input))
+
+	elapsed := time.Since(start)
+	fmt.Println("\nTime taken:", elapsed)
+}
