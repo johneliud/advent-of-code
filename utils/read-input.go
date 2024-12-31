39ceacc58f8e290e72bdeb8ea5c83ff5d598c3ab
John <johneliud4@gmail.com>
2024-12-31 14:20:44 +0300

    add: ReadInput function used to fetch puzzle input.

---
+++ utils/read-input.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+const baseURL = "https://adventofcode.com/%s/day/%s/input"
+
+// ReadInput accepts year and day as parameters in order to fetch the puzzle input from the specified URL. 
+func ReadInput(year, day string) string {
+	url := fmt.Sprintf(baseURL, year, day)
+
+	client := http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Printf("Failed to make a request: %v\n", err)
+		return ""
+	}
+
+	req.Header.Add("Cookie", "session="+os.Getenv("AOC_SESSION_COOKIE"))
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Failed to get a response: %v\n", err)
+		return ""
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("Failed reading response body: %v\n", err)
+		return ""
+	}
+	return string(body)
+}
