cbac9bc785942634d6f7c79f8d05e6946f52c157
johnodhiambo0 <johneliud4@gmail.com>
2025-01-15 04:56:48 +0300

    update: main function dynamism to accept year and day from os arguments.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"advent-of-code/2015/day1"
 	"advent-of-code/utils"
@@ -9,18 +10,29 @@ import (
 	"github.com/joho/godotenv"
 )
 
+var year, day string
+
 func main() {
+	switch len(os.Args) {
+	case 3:
+		year = os.Args[1]
+		day = os.Args[2]
+	default:
+		fmt.Println("Usage: 'go run . [YEAR] [DAY]' OR 'make [YEAR] [DAY]'")
+		return
+	}
+
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Printf("Failed loading .env file: %v\n", err)
 		return
 	}
 
-	input := utils.FetchInput("2015", "1")
+	input := utils.FetchInput(year, day)
 
-	fmt.Printf(`Day 1
+	fmt.Printf(`Day %v
 
 Part 1: %v
 Part 2: %v
-`, day1.FindFloor(input), day1.FindPosition(input))
+`, day, day1.FindFloor(input), day1.FindPosition(input))
 }
