e0b3f607c7a73ba391d7e9c60e3c4ecba67f553a
John <johneliud4@gmail.com>
2025-11-04 13:55:23 +0300

    add: base Java structure.

---
+++ Java/Utils/Utils.java
@@ -0,0 +1,6 @@
+public class Utils {
+    /*
+    Fetch the advent data from the server instead of coping the data to a file available locally.
+    */
+    public static String fetchAdventData() {}
+}
--- go.mod
+++
@@ -1,5 +0,0 @@
-module advent-of-code
-
-go 1.23.3
-
-require github.com/joho/godotenv v1.5.1
