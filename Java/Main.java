911962f5c19e4982ca29dccbcaad18471f423f17
John <johneliud4@gmail.com>
2025-11-07 18:08:35 +0300

    feat: Create main application entry point with command-line interface for fetching Advent of Code data and executing puzzle solutions with year and day parameters.

---
+++ Java/Main.java
@@ -0,0 +1,26 @@
+import utils.FetchAdventData;
+import _2015.day_one.PartOne;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java -cp build Main <year> <day>");
+            return;
+        }
+
+        String year = args[0];
+        String day = args[1];
+
+        String adventData = FetchAdventData.fetchAdventData(year, day);
+
+        if (adventData != null) {
+            System.out.println("Advent of code data fetched successfully.\n\n");
+
+            int floor = PartOne.findFloor(adventData);
+
+            System.out.println("Floor: " + floor);
+        } else {
+            System.out.println("Could not fetch Advent data for year " + year + ", day " + day + ".");
+        }
+    }
+}
